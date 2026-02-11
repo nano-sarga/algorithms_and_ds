d56495c63a7834b5656c11bfa6b0f24fcbe5edf9
nano-sarga <narrin.sarhatyan@gmail.com>
2026-02-12 02:16:55 +0400

    ceparate main/run

--- main.cpp
+++ main.cpp
@@ -1,28 +1,6 @@
-#include <iostream>
-#include "sort.h"
-#include <array>
+#include "run.h"
 
 int main() {
-    int size;
-    std::cout << "Input count of array elements:\n";
-    std::cin >> size;
-    if (size <= 0) {
-        std::cout << "Size must be > 0\n";
-        return 0;
-    }
-
-    auto* arr = new int[size];
-    std::cout << "Input array elements:\n";
-    for (int i = 0; i < size; i++) {
-        std::cin >> arr[i];
-    }
-    bubble_sort(arr, size);
-
-    std::cout << "Array after bubble sorting:\n";
-    for (int i = 0; i < size; i++) {
-        std::cout << arr[i] << ' ';
-    }
-
-    delete[] arr;
+    run_bubble();
     return 0;
 }
